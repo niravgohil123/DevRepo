9c9b58fa4f9d2542d8a24386925895684577afb7
EC2 Default User <ec2-user@ip-172-31-23-32.us-east-2.compute.internal>
2019-09-21 09:02:06 +0000

    aws create inst

---
+++ aws.sh
@@ -0,0 +1,6 @@
+aws ec2 run-instances \
+    --image-id ami-00c03f7f7f2ec15c3 \
+    --instance-type t2.micro \
+    --key-name First_Instance \
+    --security-group-ids sg-0b270d835c1bf1b47 \
+    --subnet-id subnet-041512ba1a3491a8c
